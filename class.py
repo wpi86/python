49d35354aa4f2cdcb61dc586a5538718e21dae44
Pavel Varfolomeev <varfolomeev@soramitsu.co.jp>
2022-05-09 13:07:47 +0300

    learn classes

---
+++ class.py
@@ -0,0 +1,16 @@
+class Car:
+    speed = 0
+    started = False
+    def start(self):
+        self.started = True
+        print("Car started, let's ride!")
+    def increase_speed(self, delta):
+        if self.started:
+            self.speed = self.speed + delta
+            print('Vrooooom!')
+        else:
+            print("You need to start the car first")
+    def stop(self):
+        self.speed = 0
+        print('Halting')
+
